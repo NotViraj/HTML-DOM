a89cfe9a23b1545713616d9240c725dc88596033
NotViraj <virajsinghd1@gmail.com>
2025-03-26 14:33:36 +0530

    hello

---
+++ script2.js
@@ -0,0 +1,6 @@
+function z() {
+    document.getElementById("x").innerHTML=
+    document.getElementById("abc").firstChild.nodeValue;
+    document.getElementById("y").innerHTML=
+    document.getElementById("abc").childNodes[0].nodeValue;
+}
